c82298c7092c93b022c93b2d18ed1c3de1623d72
Hari31416 <harikeshkumar0926@gmail.com>
2021-05-31 09:34:50 +0530

    Problems

--- Codes/Coding Challenges/myCodes.js
+++ Codes/Coding Challenges/myCodes.js
@@ -1,17 +1,21 @@
-let isPrimeCheck = false;
+var t0 = performance.now();
 function isPrime(n) {
-  if (n === 1 || n === 2 || n % 2 === 0) {
-    isPrimeCheck = false;
+  if (n === 1 || n % 2 === 0) {
     if (n === 2) {
-      isPrimeCheck = true;
+      return true;
+    }
+    return false;
+  } else if (n === 2) {
+    return true;
+  } else {
+    for (let i = 3; i < Math.floor(Math.sqrt(n)) + 1; i = i + 2) {
+      if (n % i === 0) {
+        return false;
+      }
     }
   }
-  for (let i = 3; i <= n; i = i + 2) {
-    if (n % i === 0) {
-      isPrimeCheck = false;
-    }
-  }
-  isPrimeCheck = true;
+  return true;
 }
-isPrime(10);
-console.log(isPrimeCheck);
+console.log(isPrime(4256233));
+var t1 = performance.now();
+console.log("It took " + (t1 - t0) * 100 + " milliseconds.");
