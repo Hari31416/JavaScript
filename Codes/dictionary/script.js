a98546768147a301843ec9beb0f3fa020533b24c
Hari31416 <harikeshkumar0926@gmail.com>
2021-06-30 21:47:13 +0530

    Dictionary

--- Codes/dictionary/script.js
+++ Codes/dictionary/script.js
@@ -1,22 +1,44 @@
 word = document.querySelectorAll(".word");
 meaning = document.querySelectorAll(".meaning");
 close = document.querySelectorAll(".close");
-// // console.log(word);
-// const open = function (i) {
-//   meaning[i].classList.remove("hidden");
-// };
-// meaning[0].addEventListener("click", open(0));
-// console.log(meaning[0]);
-// for (let i = 0; i < word.length; i++) {
-//   word[i].addEventListener("click", open);
-// }
-console.log(word.length);
+reset = document.querySelectorAll(".reset");
+console.log(reset);
 for (let i = 0; i < word.length; i++) {
+  // Showing the meaning
   word[i].addEventListener("click", function () {
-    meaning[i].classList.remove("hidden");
+    meaning[i].classList.toggle("hidden");
+    close[i].classList.toggle("hidden");
+    word[i].classList.toggle("clicked");
   });
 
+  // Hiding the meaning by close button
   close[i].addEventListener("click", function () {
     meaning[i].classList.add("hidden");
+    close[i].classList.add("hidden");
+    word[i].classList.remove("clicked");
+  });
+
+  // Hovering over the words
+  word[i].addEventListener("mouseover", function () {
+    word[i].classList.add("hover");
+  });
+
+  word[i].addEventListener("mouseout", function () {
+    word[i].classList.remove("hover");
+  });
+
+  // Hovering over the close button
+  close[i].addEventListener("mouseover", function () {
+    close[i].classList.add("hoverClose");
+  });
+
+  close[i].addEventListener("mouseout", function () {
+    close[i].classList.remove("hoverClose");
+  });
+
+  reset[0].addEventListener("click", function () {
+    meaning[i].classList.add("hidden");
+    close[i].classList.add("hidden");
+    word[i].classList.remove("clicked");
   });
 }
