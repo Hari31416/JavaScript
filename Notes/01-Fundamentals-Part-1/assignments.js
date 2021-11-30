85e47a4d9860da9c44d08ff3f39453e2510c954d
Hari31416 <harikeshkumar0926@gmail.com>
2021-11-30 13:27:19 +0530

    Some Reorganization and Addition

--- Notes/01-Fundamentals-Part-1/assignments.js
+++
@@ -1,7 +0,0 @@
-console.log(4 % 2);
-console.log(12 + 12 ** 2);
-console.log("Hello There");
-
-var value = 23;
-console.log(typeof value);
-console.log(2 + 2);
---
+++ Notes/Notes 1/01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,7 @@
+console.log(4 % 2);
+console.log(12 + 12 ** 2);
+console.log("Hello There");
+
+var value = 23;
+console.log(typeof value);
+console.log(2 + 2);
